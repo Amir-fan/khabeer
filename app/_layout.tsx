5b038d755bb46b5f29eda70ac8f1a36ff17a58f9
Amir-fan <amir.f.fanari@gmail.com>
2026-01-09 01:17:03 +0300

    Remove tabbed routes to fix post-login flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -88,7 +88,6 @@ export default function RootLayout() {
           <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="onboarding" />
             <Stack.Screen name="auth" />
-            <Stack.Screen name="(tabs)" />
             <Stack.Screen name="oauth/callback" />
             <Stack.Screen name="stock" />
             <Stack.Screen name="contract" />
