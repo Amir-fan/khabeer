a2e6430086f3e9955847ff5a4b92a7f0a5728f9a
Amir-fan <amir.f.fanari@gmail.com>
2026-01-08 17:30:12 +0300

    Fix vercel.json: Remove conflicting functions property, use builds config instead

--- api/index.ts
+++ api/index.ts
@@ -3,18 +3,20 @@
  * 
  * This file serves as the entry point for Vercel serverless functions.
  * It exports the Express app as a serverless function.
+ * 
+ * Max duration: 30 seconds (configured via vercel.json)
  */
 
 import "dotenv/config";
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "../server/_core/oauth.js";
-import { appRouter } from "../server/routers.js";
-import { createContext } from "../server/_core/context.js";
-import { validateEnvironmentOnStartup } from "../server/_core/envValidation.js";
-import { initErrorMonitoring } from "../server/_core/monitoring.js";
-import { seedTierLimits } from "../server/db.js";
-import { seedAccounts } from "../server/_core/seedAccounts.js";
+import { registerOAuthRoutes } from "../server/_core/oauth";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
+import { validateEnvironmentOnStartup } from "../server/_core/envValidation";
+import { initErrorMonitoring } from "../server/_core/monitoring";
+import { seedTierLimits } from "../server/db";
+import { seedAccounts } from "../server/_core/seedAccounts";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -112,8 +114,8 @@ async function initializeServer() {
 }
 
 // Vercel serverless function handler
+// Max duration is configured in vercel.json builds config
 export default async function handler(req: express.Request, res: express.Response) {
   const serverApp = await initializeServer();
   return serverApp(req, res);
 }
-
